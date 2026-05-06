df2210412a11f28b97b6091dfa75e750b1457349
GnosisDodecaedru <gabriel_aguitoni@hotmail.com>
2026-05-06 20:29:41 -0300

    Refactor addTask function and add task management features
    Fix task creation and add functionality to edit, remove, and mark tasks as completed.

--- jv.js
+++ jv.js
@@ -5,14 +5,27 @@ const taskList = document.getElementById('taskList');
 function addTask(){
   const textItem = taskInput.value.trim();
   if (textItem != ''){
-    const newTask - document.createElement('li');
-    newTask.innerHTML = '
-      <span>$(textItem)</span>
-      <button onclick="">Concluir</button>
-      <button onclick="">Editar</button>
-      <button onclick="">Remover</button>    
-      ' ;
-  }
+    const newTask = document.createElement('li');
+    newTask.innerHTML = `
+      <span>${textItem}</span>
+      <button onclick="editar()">Editar</button>
+      <button onclick="delet(this)">Remover</button>
+      <button onclick="feito(this)">feito</button>
+       `;
+  
   taskList.appendChild(newTask);
   taskInput.value = '';
+  }
+}
+
+
+
+function delet(button){
+ const taskToRemove = button.parentElement; 
+ taskList.removeChild(taskToRemove);  
+}
+function feito(button){
+  const taskComplete = button.parentElement;
+  taskComplete.classList.add('completed');
+  
 }
